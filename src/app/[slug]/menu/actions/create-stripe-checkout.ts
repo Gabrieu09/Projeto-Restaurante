97b6af7c390c661031f48c81250a381f47b14475
Gabrieu09 <biel.santos2202@gmail.com>
2025-04-11 11:51:59 -0300

    correção pagamentos

--- src/app/[slug]/menu/actions/create-stripe-checkout.ts
+++ src/app/[slug]/menu/actions/create-stripe-checkout.ts
@@ -4,31 +4,48 @@ import { ConsumptionMethod } from "@prisma/client";
 import { headers } from "next/headers";
 import Stripe from "stripe";
 
-import { CartProduct } from "../contexts/cart";
+import { db } from "@/lib/prisma";
 
-export const createStripeCheckout = async ({
-  orderId,
-  slug,
-  consumptionMethod,
-  products,
-}: {
+import { CartProduct } from "../contexts/cart";
+import { removeCpfPunctuation } from "../helpers/cpf";
+
+interface CreateStripeCheckoutInput {
+  products: CartProduct[];
   orderId: number;
   slug: string;
   consumptionMethod: ConsumptionMethod;
-  products: CartProduct[];
-}) => {
-  const origin = (await headers()).get("origin") || "";
+  cpf: string;
+}
+
+export const createStripeCheckout = async ({
+  orderId,
+  products,
+  slug,
+  consumptionMethod,
+  cpf,
+}: CreateStripeCheckoutInput) => {
   if (!process.env.STRIPE_SECRET_KEY) {
-    throw new Error("Stripe secret key not found");
+    throw new Error("Missing Stripe secret key");
   }
+  const origin = (await headers()).get("origin") as string;
+  const productsWithPrices = await db.product.findMany({
+    where: {
+      id: {
+        in: products.map((product) => product.id),
+      },
+    },
+  });
   const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
     apiVersion: "2025-02-24.acacia",
   });
+  const searchParams = new URLSearchParams();
+  searchParams.set("consumptionMethod", consumptionMethod);
+  searchParams.set("cpf", removeCpfPunctuation(cpf));
   const session = await stripe.checkout.sessions.create({
-    payment_method_types: ["card", "boleto"],
+    payment_method_types: ["card"],
     mode: "payment",
-    success_url: `${origin}/${slug}?consumptionMethod=${consumptionMethod}`,
-    cancel_url: `${origin}/${slug}?consumptionMethod=${consumptionMethod}`,
+    success_url: `${origin}/${slug}/orders?${searchParams.toString()}`,
+    cancel_url: `${origin}/${slug}/orders?${searchParams.toString()}`,
     metadata: {
       orderId,
     },
@@ -39,7 +56,8 @@ export const createStripeCheckout = async ({
           name: product.name,
           images: [product.imageUrl],
         },
-        unit_amount: product.price * 100,
+        unit_amount:
+          productsWithPrices.find((p) => p.id === product.id)!.price * 100,
       },
       quantity: product.quantity,
     })),
